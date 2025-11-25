0724afdf77e593c02f1cb7ed82e82f7b9f26fb4a
Aminul Islam <cselab7777@gmail.com>
2025-11-25 16:26:53 +0600

    Finally Bind interface  with repo

--- src/CrudGenerateServiceProvider.php
+++ src/CrudGenerateServiceProvider.php
@@ -26,6 +26,36 @@ class CrudGenerateServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/resources/stubs' => resource_path('vendor/aminul/stubs'),
         ]);
+
+        // Auto-bind repositories
+        $this->autoBindRepositories();
+    }
+
+    /**
+     * Automatically bind repository interfaces for generated CRUDs
+     */
+    protected function autoBindRepositories(): void
+    {
+        $appNamespace = trim($this->app->getNamespace(), '\\');
+        $contractsPath = app_path('Repositories/Contracts');
+
+        if (!is_dir($contractsPath)) {
+            return;
+        }
+
+        $interfaceFiles = glob($contractsPath . '/*RepositoryInterface.php');
+
+        foreach ($interfaceFiles as $interfaceFile) {
+            $interfaceName = basename($interfaceFile, '.php');
+            $modelName = str_replace('RepositoryInterface', '', $interfaceName);
+
+            $interfaceClass = "{$appNamespace}\\Repositories\\Contracts\\{$interfaceName}";
+            $repositoryClass = "{$appNamespace}\\Repositories\\{$modelName}Repository";
+
+            if (interface_exists($interfaceClass) && class_exists($repositoryClass)) {
+                $this->app->bind($interfaceClass, $repositoryClass);
+            }
+        }
     }
 
 }
