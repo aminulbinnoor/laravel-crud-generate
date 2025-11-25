6600a7d2be966920c3b6d6571c3c50ff1f49ec78
Aminul Islam <cselab7777@gmail.com>
2025-11-25 16:13:39 +0600

    update service provider

--- src/CrudGenerateServiceProvider.php
+++ src/CrudGenerateServiceProvider.php
@@ -13,9 +13,6 @@ class CrudGenerateServiceProvider extends ServiceProvider
         $this->commands([
             GenerateCrudCommand::class,
         ]);
-
-        // Auto-bind repositories
-        $this->bindRepositories();
     }
 
     public function boot()
@@ -31,43 +28,4 @@ class CrudGenerateServiceProvider extends ServiceProvider
         ]);
     }
 
-    /**
-     * Automatically bind repository interfaces with their implementations.
-     *
-     * @return void
-     */
-    protected function bindRepositories()
-    {
-        // Get the application namespace
-        $namespace = $this->app->getNamespace();
-
-        // Define the paths for interfaces and repositories
-        $interfacePath = app_path('Repositories/Contracts');
-        $repositoryPath = app_path('Repositories');
-
-        // Check if the directories exist
-        if (!is_dir($interfacePath) || !is_dir($repositoryPath)) {
-            return;
-        }
-
-        // Get all interface files
-        $interfaceFiles = glob($interfacePath . '/*RepositoryInterface.php');
-
-        foreach ($interfaceFiles as $interfaceFile) {
-            // Extract interface name from filename
-            $interfaceName = basename($interfaceFile, '.php');
-
-            // Extract model name from interface name (remove 'RepositoryInterface')
-            $modelName = str_replace('RepositoryInterface', '', $interfaceName);
-
-            // Build full class names
-            $interfaceClass = "{$namespace}Repositories\\Contracts\\{$interfaceName}";
-            $repositoryClass = "{$namespace}Repositories\\{$modelName}Repository";
-
-            // Check if both classes exist before binding
-            if (class_exists($interfaceClass) && class_exists($repositoryClass)) {
-                $this->app->bind($interfaceClass, $repositoryClass);
-            }
-        }
-    }
 }
